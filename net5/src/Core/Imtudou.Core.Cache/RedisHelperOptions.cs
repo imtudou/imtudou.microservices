746ac672bd320fcf79193ded408d0bb3be875d45
yuanyi <imtudous@163.com>
2021-11-15 19:39:36 +0800

    redishelper update

---
+++ net5/src/Core/Imtudou.Core.Cache/RedisHelperOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imtudou.Core.Cache
+{
+    public class RedisHelperOptions
+    {
+        [Required(ErrorMessage = "redis connection string is required")]
+        public string ConnectionString { get; set; }
+
+        [Range(0, 15, ErrorMessage = "redis db number must be between 0 and 15")]
+        public int DbNumber { get; set; }
+
+        public RedisHelperOptions()
+        {
+        }
+
+        public RedisHelperOptions(string connectionString, int dbNumber)
+        {
+            ConnectionString = connectionString;
+            DbNumber = dbNumber;
+        }
+    }
+}
